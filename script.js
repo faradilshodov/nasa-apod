55310bb9eb26121e6c9b7266741e3ef1f62abd3d
Farrukh(Fara) Dilshodov <farrukh.dilshodov@gmail.com>
2025-05-28 10:37:59 -0400

    Added favorites DOM elements

--- script.js
+++ script.js
@@ -12,8 +12,10 @@ const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${co
 let resultsArray = [];
 let favorites = {};
 
-function updateDOM() {
-    resultsArray.forEach((result) => {
+function createDOMNodes(page) {
+    const currentArray = page === 'results' ? resultsArray : Object.values(favorites);
+
+    currentArray.forEach((result) => {
         // Card Container
         const card = document.createElement('div');
         card.classList.add('card');
@@ -62,13 +64,20 @@ function updateDOM() {
     });
 }
 
+function updateDOM(page) {
+    // Get Favorites from Local Storage
+    if (localStorage.getItem('nasaFavorites')) {
+        favorites = JSON.parse(localStorage.getItem('nasaFavorites'));
+    }
+    createDOMNodes(page);
+}
+
 // Get 10 Images from NASA API
 async function getNasaPictures() {
   try {
     const response = await fetch(apiUrl);
     resultsArray = await response.json();
-    console.log(resultsArray);
-    updateDOM();
+    updateDOM('favorites');
   } catch (error) {
     // Catch error here
     console.error('Error fetching data from NASA API:', error);
